2a94f5918994a962a569e18b76c8b2109f4299ba
Abhishek <abhishekkannur31@gmail.com>
2026-06-15 06:26:10 +0530

    fix: remove huge gap caused by MeshGradientDivider blocking layout

--- components/ui/MeshGradientDivider.tsx
+++ components/ui/MeshGradientDivider.tsx
@@ -1,6 +1,6 @@
 export function MeshGradientDivider() {
   return (
-    <div className="relative h-[300px] w-full overflow-hidden pointer-events-none">
+    <div className="absolute top-0 left-0 w-full h-[300px] pointer-events-none -translate-y-1/2 overflow-hidden z-[-1]">
       <div 
         className="absolute left-0 top-1/2 w-3/5 h-full -translate-y-1/2"
         style={{
